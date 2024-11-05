e76d5a2533c9c3f9891b997027959ac34d0ae717
dariu <darius.studiosuipod@gmail.com>
2024-11-05 13:52:01 -0600

    feat: input error messages appear for all fields

---
+++ scripts/validation.js
@@ -0,0 +1,35 @@
+// Error Messages
+
+const showInputError = (formElement, inputElement, errorMsg) => {
+  const errorMsgElement = document.querySelector(`#${inputElement.id}-error`);
+  errorMsgElement.textContent = errorMsg;
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll(".modal__input"));
+  const buttonElement = formElement.querySelector(".modal__btn");
+
+  // toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formElement, inputElement);
+      // toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = document.querySelectorAll(".modal__form");
+  formList.forEach((formEl) => {
+    setEventListeners(formEl);
+  });
+};
+
+enableValidation();
